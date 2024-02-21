1829ee9080ef2a3361167b99a43439399282fea8
Lulu Bouron <lce.bouron@gmail.com>
2024-02-21 15:36:04 +0100

    modification sur le form pour inscrire un user

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,9 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,18 +20,45 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('full_name', TextType::class, [
+                'label'=>false,
+                'attr'=>[
+                    'placeholder' => 'Enter Your Full Name',
+                    'class' => 'form-control'
+                ],
+                'row_attr'=> [
+                    'class'=>'form-group mb-3'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label'=>false,
+                'attr'=>[
+                    'placeholder' => 'Enter Your Email',
+                    'class' => 'form-control'
+                ],
+                'row_attr'=> [
+                    'class'=>'form-group mb-3'
+                ]
+            ])
+          
             ->add('agreeTerms', CheckboxType::class, [
-                                'mapped' => false,
+                'mapped' => false,
+                'attr'=>[
+                    'class'=>'form-check-input'
+                ],
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
                     ]),
                 ],
+                'row_attr'=>[
+                    'class' => 'custome-checkbox d-flex gap-2'
+                ]
             ])
-            ->add('plainPassword', PasswordType::class, [
-                                // instead of being set onto the object directly,
+            ->add('plainPassword', RepeatedType::class, [
+                // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'type' => PasswordType::class,
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
@@ -40,8 +70,29 @@ class RegistrationFormType extends AbstractType
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
-                    ]),
+                    ])
                 ],
+                'first_options'=>[
+                        'label'=>false,
+                        'attr'=>[
+                            'placeholder' => 'Your Password please ...',
+                            'class' => 'form-control'
+                        ],
+                        'row_attr'=> [
+                            'class'=>'form-group mb-3'
+                        ]
+                    ],
+                    'second_options'=>[
+                        'label'=>false,
+                        'attr'=>[
+                            'placeholder' => 'Confirm Your Password please ...',
+                            'class' => 'form-control'
+                        ],
+                        'row_attr'=> [
+                            'class'=>'form-group mb-3'
+                        ]
+                    ]
+                
             ])
         ;
     }
