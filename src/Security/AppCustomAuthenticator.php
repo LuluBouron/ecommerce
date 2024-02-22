a5ae3b0a0e769c864819394bd5d46df2d1451e48
Lulu Bouron <lce.bouron@gmail.com>
2024-02-22 14:42:23 +0100

    page account and rediret whjen authentificated

--- src/Security/AppCustomAuthenticator.php
+++ src/Security/AppCustomAuthenticator.php
@@ -49,7 +49,7 @@ class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
         }   
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('app_home'));
+        return new RedirectResponse($this->urlGenerator->generate('app_account'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
